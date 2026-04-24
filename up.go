da2dd039ecae9525e175387428bc403a61555f8a
Theis Mejnertsen <theismejnertsen@gmail.com>
2026-04-24 13:59:07 +0200

    feat!: default to attaching to session on up
    Mimicking docker compose behaviour
    
    Signed-off-by: Theis Mejnertsen <theismejnertsen@gmail.com>

--- up.go
+++ up.go
@@ -5,9 +5,19 @@ import "github.com/spf13/cobra"
 const kCmdUp = "up"
 
 func NewUpCommand() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   kCmdUp,
 		Short: "Create and start the server and sessions",
-		Run:   func(cmd *cobra.Command, args []string) { initCmd(cmd, args).up() },
+		Run: func(cmd *cobra.Command, args []string) {
+			project := initCmd(cmd, args)
+			project.up()
+			if !cmd.Flag("detach").Changed {
+				project.attach()
+			}
+		},
 	}
+
+	cmd.PersistentFlags().BoolP("detach", "d", false, "Detach from tmux session after starting it.")
+
+	return cmd
 }
